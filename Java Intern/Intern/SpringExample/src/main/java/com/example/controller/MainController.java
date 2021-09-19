5b62e64d53a1fe7a1b915585fbedb23878cbaf33
inrusakov <inrusakov@edu.hse.ru>
2021-09-19 18:36:17 +0300

    upd

--- Java Intern/Intern/SpringExample/src/main/java/com/example/controller/MainController.java
+++ Java Intern/Intern/SpringExample/src/main/java/com/example/controller/MainController.java
@@ -4,7 +4,6 @@ import com.example.model.User;
 import com.example.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
